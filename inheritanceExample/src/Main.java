5c140a5a49fe4eb2e5782d82b8bf9bba2a31ef88
OstapMykhailiv <115294426+OstapMykhailiv@users.noreply.github.com>
2023-01-07 18:08:33 +0200

    Add files via upload

---
+++ inheritanceExample/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+    call myAnimal=new call();
+    myAnimal.animal("dog", "golden retriever");
+    myAnimal.animalCall("Cooper");
+    }
+}
---
+++ inheritanceExample/src/animal.java
@@ -0,0 +1,6 @@
+public class animal {
+    public void animal(String specie, String breed)
+    {
+        System.out.print("The animal is " + specie+", "+"the breed is "+breed);
+    }
+}
