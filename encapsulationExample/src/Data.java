d0f50f6acd7bfcccdffea35b589be23ff7258a5c
OstapMykhailiv <115294426+OstapMykhailiv@users.noreply.github.com>
2023-01-07 17:43:01 +0200

    Add files via upload

---
+++ encapsulationExample/src/Data.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Data {
+
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        Scanner input = new Scanner(System.in);
+        this.name=input.next();
+    }
+
+    private String surname;
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname()
+    {
+        Scanner input = new Scanner(System.in);
+        this.surname=input.next();
+    }
+
+    private int age;
+    public int getAge(){
+        return age;
+    }
+    public void setAge(){
+        Scanner input = new Scanner(System.in);
+        this.age= input.nextInt();
+    }
+    private int height;
+    public int getHeight(){
+        return height;
+    }
+    public void setHeight(){
+        Scanner input = new Scanner(System.in);
+        this.height= input.nextInt();
+    }
+
+    private String country;
+
+    public String getCountry(){
+        return country;
+    }
+
+    public void setCountry(){
+        Scanner input = new Scanner(System.in);
+        this.country = input.nextLine();
+    }
+
+}
